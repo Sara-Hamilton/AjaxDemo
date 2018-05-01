f95f2dc99db87fb18129fe2945f0fc15b16d323e
Sara Hamilton and Stephanie Faber <sara@indeterminate.net and eyesicedover@gmail.com>
2018-05-01 07:59:06 -0700

    initial commit

--- AjaxDemo/Startup.cs
+++ AjaxDemo/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,13 +13,25 @@ namespace AjaxDemo
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+      
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            //services.AddEntityFrameworkMySql()
+            //.AddDbContext<ToDoListContext>(options =>
+            //                          options
+            //                               .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
@@ -27,6 +40,14 @@ namespace AjaxDemo
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
